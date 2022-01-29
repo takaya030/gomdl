3646615b221855a8d12f75e72869381603dae0df
Minoru Takayama <takaya030@yahoo.co.jp>
2022-01-29 16:24:27 +0900

    add go.mod

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/takaya030/gomdl
+
+go 1.17
+
+require (
+	github.com/go-gl/mathgl v1.0.0
+	github.com/kr/pretty v0.3.0
+)
+
+require (
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
+	golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f // indirect
+)
--- main.go
+++ main.go
@@ -4,20 +4,28 @@ import (
 	"fmt"
 	"github.com/kr/pretty"
 	"io/ioutil"
+	"unsafe"
 
-	"github.com/takaya030/gomdl/mdl"
+	//"github.com/takaya030/gomdl/mdl"
+	"github.com/takaya030/gomdl/studio"
 )
 
 func main() {
-	buf, rferr := ioutil.ReadFile(`../asset/sas.mdl`)
+	buf, rferr := ioutil.ReadFile(`./asset/gsg9.mdl`)
 	if rferr != nil {
 		fmt.Print(rferr)
 		return
 	}
 
+	// cast hdr
+	p_Hdr := (*studio.Hdr)(unsafe.Pointer(&buf[0]))
+	fmt.Printf("%# v\n", pretty.Formatter(*p_Hdr))
+
 	// read mdldata
+	/*
 	md := mdl.NewMdlData(buf)
 	fmt.Printf("%# v\n", pretty.Formatter(*md))
+	*/
 
 	// read seqdescs
 	/*
