ccde063e23edd3edd1d030dfebe621cb2f74b616
Minoru Takayama <takaya030@yahoo.co.jp>
2022-02-05 13:24:48 +0900

    add sequence getter, setter

--- mdl/mdlmodel.go
+++ mdl/mdlmodel.go
@@ -12,6 +12,9 @@ import (
 // calc utility
 type MdlModel struct {
 	mdd	*MdlData
+
+	sequence	int32
+	frame 		float32
 }
 
 func NewMdlModel(mdd *MdlData) *MdlModel {
@@ -19,4 +22,21 @@ func NewMdlModel(mdd *MdlData) *MdlModel {
 	mdm.mdd = mdd
 
 	return mdm
+}
+
+func (mm *MdlModel) GetSequence() int32 {
+	return mm.sequence
+}
+
+func (mm *MdlModel) SetSequence(iseq int32) int32 {
+	if iseq >= mm.mdd.NumSeq() {
+		iseq = 0
+	} else if iseq < 0 {
+		iseq = mm.mdd.NumSeq() - 1
+	}
+
+	mm.sequence = iseq
+	mm.frame = 0
+
+	return mm.sequence
 }
