cef1726eb0c26307c8adf5f51fdc7db1ac86b4c5
Minoru Takayama <takaya030@yahoo.co.jp>
2022-02-05 17:03:37 +0900

    add SetController

--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/takaya030/gomdl
 go 1.17
 
 require (
-	github.com/go-gl/mathgl v1.0.0
+	github.com/chewxy/math32 v1.10.1
 	github.com/kr/pretty v0.3.0
 )
 
 require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.6.1 // indirect
-	golang.org/x/image v0.0.0-20190321063152-3fc05d484e9f // indirect
 )
--- mdl/mdlmodel.go
+++ mdl/mdlmodel.go
@@ -6,7 +6,8 @@ import (
 	//"fmt"
 	//"github.com/go-gl/mathgl/mgl32"
 
-	//"github.com/takaya030/gomdl/studio"
+	"github.com/chewxy/math32"
+	"github.com/takaya030/gomdl/studio"
 )
 
 // calc utility
@@ -15,6 +16,7 @@ type MdlModel struct {
 
 	sequence	int32
 	frame 		float32
+	controller	[4]uint8
 }
 
 func NewMdlModel(mdd *MdlData) *MdlModel {
@@ -39,4 +41,58 @@ func (mm *MdlModel) SetSequence(iseq int32) int32 {
 	mm.frame = 0
 
 	return mm.sequence
+}
+
+func (mm *MdlModel) SetController(icntl int32, flval float32) float32 {
+	var bc *studio.BoneController = nil
+
+	// find first controller that matches the index
+	for i := 0; i < (int)(mm.mdd.GetNumBoneControllers()); i++ {
+		tmpbc := mm.mdd.GetBoneController(i)
+		if tmpbc.Index == icntl {
+			bc = tmpbc
+			break
+		}
+	}
+
+	if bc == nil {
+		return flval
+	}
+
+	// wrap 0..360 if it's a rotational controller
+	if (bc.Type & (studio.STUDIO_XR | studio.STUDIO_YR | studio.STUDIO_ZR)) != 0 {
+
+		// invert value if end < start
+		if bc.End < bc.Start {
+			flval = -flval
+		}
+
+		// does the controller not wrap?
+		if bc.Start + 359.0 >= bc.End {
+			if flval > ((bc.Start + bc.End) / 2.0) + 180 {
+				flval = flval - 360
+			}
+			if flval < ((bc.Start + bc.End) / 2.0) - 180 {
+				flval = flval + 360
+			}
+		} else {
+			if flval > 360 {
+				flval = flval - math32.Floor(flval / 360.0) * 360.0
+			} else if flval < 0 {
+				flval = flval + math32.Floor((flval / -360.0) + 1) * 360.0
+			}
+		}
+	}
+
+	var setting int = (int)(math32.Floor(255 * (flval - bc.Start) / (bc.End - bc.Start)))
+
+	if setting < 0 {
+		setting = 0
+	}
+	if setting > 255 {
+		setting = 255
+	}
+	mm.controller[icntl] = (uint8)(setting)
+
+	return (float32)(setting) * (1.0 / 255.0) * (bc.End - bc.Start) + bc.Start
 }
