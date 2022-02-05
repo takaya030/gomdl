19ab64b90c9bd8ba5ee4704b02bf8848864362be
Minoru Takayama <takaya030@yahoo.co.jp>
2022-02-05 17:28:50 +0900

    add SetMouth

--- mdl/mdlmodel.go
+++ mdl/mdlmodel.go
@@ -17,6 +17,7 @@ type MdlModel struct {
 	sequence	int32
 	frame 		float32
 	controller	[4]uint8
+	mouth		uint8
 }
 
 func NewMdlModel(mdd *MdlData) *MdlModel {
@@ -95,4 +96,58 @@ func (mm *MdlModel) SetController(icntl int32, flval float32) float32 {
 	mm.controller[icntl] = (uint8)(setting)
 
 	return (float32)(setting) * (1.0 / 255.0) * (bc.End - bc.Start) + bc.Start
+}
+
+func (mm *MdlModel) SetMouth(flval float32) float32 {
+	var bc *studio.BoneController = nil
+
+	// find first controller that matches the index
+	for i := 0; i < (int)(mm.mdd.GetNumBoneControllers()); i++ {
+		tmpbc := mm.mdd.GetBoneController(i)
+		if tmpbc.Index == 4 {
+			bc = tmpbc
+			break
+		}
+	}
+
+	if bc == nil {
+		return flval
+	}
+
+	// wrap 0..360 if it's a rotational controller
+	if (bc.Type & (studio.STUDIO_XR | studio.STUDIO_YR | studio.STUDIO_ZR)) != 0 {
+
+		// invert value if end < start
+		if bc.End < bc.Start {
+			flval = -flval
+		}
+
+		// does the controller not wrap?
+		if bc.Start + 359.0 >= bc.End {
+			if flval > ((bc.Start + bc.End) / 2.0) + 180 {
+				flval = flval - 360
+			}
+			if flval < ((bc.Start + bc.End) / 2.0) - 180 {
+				flval = flval + 360
+			}
+		} else {
+			if flval > 360 {
+				flval = flval - math32.Floor(flval / 360.0) * 360.0
+			} else if flval < 0 {
+				flval = flval + math32.Floor((flval / -360.0) + 1) * 360.0
+			}
+		}
+	}
+
+	var setting int = (int)(math32.Floor(64 * (flval - bc.Start) / (bc.End - bc.Start)))
+
+	if setting < 0 {
+		setting = 0
+	}
+	if setting > 64 {
+		setting = 64
+	}
+	mm.mouth = (uint8)(setting)
+
+	return (float32)(setting) * (1.0 / 64.0) * (bc.End - bc.Start) + bc.Start
 }
