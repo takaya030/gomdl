884c442d9f56a00d21fd6396042690027a36eb0a
Minoru Takayama <takaya030@yahoo.co.jp>
2022-02-12 09:04:51 +0900

    add SetupLighting

--- mdl/mdlmodel.go
+++ mdl/mdlmodel.go
@@ -10,6 +10,10 @@ import (
 	"github.com/takaya030/gomdl/studio"
 )
 
+const (
+	MAXSTUDIOBONES	= 128		// total bones actually used
+)
+
 // calc utility
 type MdlModel struct {
 	mdd	*MdlData
@@ -25,6 +29,15 @@ type MdlModel struct {
 	// internal data
 	pmodel		*studio.Model
 	adj			studio.Vec4
+
+	// for test
+	g_lightvec		studio.Vec3
+	g_lightcolor	studio.Vec3
+	g_blightvec		[MAXSTUDIOBONES]studio.Vec3
+	g_ambientlight	float32		// ambient world light
+	g_shadelight	float32		// direct world light
+
+	g_bonetransform		[MAXSTUDIOBONES]studio.Mat34	// bone transformation matrix
 }
 
 func NewMdlModel(mdd *MdlData) *MdlModel {
@@ -222,6 +235,20 @@ func (mm *MdlModel) AdvanceFrame(dt float32) {
 		// wrap
 		mm.frame -= math32.Floor(mm.frame / (float32)(seq.NumFrames - 1)) * (float32)(seq.NumFrames - 1)
 	}
+}
+
+func (mm *MdlModel) SetupLighting() {
+	mm.g_ambientlight = 32.0
+	mm.g_shadelight = 192.0
+
+	mm.g_lightvec = studio.Vec3{ 0, 0, -1.0 }
+
+	mm.g_lightcolor = studio.Vec3{ 1.0, 1.0, 1.0 }
+
+	for i := 0; i < (int)(mm.mdd.GetNumBones()); i++ {
+		mm.g_lightvec.VectorIRotate( &(mm.g_bonetransform[i]), &(mm.g_blightvec[i]) )
+	}
+
 }
 
 func (mm *MdlModel) SetupModel(bodypart int32) {
