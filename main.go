3b23da0fa0ecbbc087cc6f419fc6f95b272c6d2b
Minoru Takayama <takaya030@yahoo.co.jp>
2022-02-23 12:36:08 +0900

    modify main.go

--- main.go
+++ main.go
@@ -32,7 +32,13 @@ func main() {
 	mdm.AdvanceFrame(0.01)
 	mdm.SetupModel(0)
 	//fmt.Printf("%# v\n", pretty.Formatter(mdd.GetSeqDesc(0)))
-	fmt.Printf("%# v\n", pretty.Formatter(*mdm))
+	fmt.Printf("%# v\n", pretty.Formatter(*(mdd.Hdr)))
+
+	var tex [3]*studio.Texture
+	tex[0] = mdd.GetTexture(0)
+	tex[1] = mdd.GetTexture(1)
+	tex[2] = mdd.GetTexture(2)
+	fmt.Printf("%# v\n", pretty.Formatter(tex))
 
 	vec1 := studio.Vec3{ 1.0, 1.0, 0.0 }
 	vec2 := studio.Vec3{ 2.0, 0.0, 0.0 }
