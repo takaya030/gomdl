8aae546d454527b94642fd969548fecbc39ef21d
Minoru Takayama <takaya030@yahoo.co.jp>
2022-02-23 16:48:47 +0900

    add Rgb struct

--- studio/texture.go
+++ studio/texture.go
@@ -1,6 +1,7 @@
 package studio
 
 import (
+	"unsafe"
 	//"github.com/go-gl/mathgl/mgl32"
 )
 
@@ -11,6 +12,10 @@ type Texture struct {
 	Width  int32
 	Height int32
 	Index  int32
+}
+
+type Rgb struct {
+	r, g, b byte
 }
 
 func (tx *Texture) GetPixelBuf(basebuf []byte) []byte {
@@ -25,4 +30,10 @@ func (tx *Texture) GetPalBuf(basebuf []byte) []byte {
 	ed := (int)(tx.Index) + (int)(tx.Width * tx.Height) + (256 * 3)
 
 	return basebuf[st:ed]
+}
+
+func (tx *Texture) GetRgb( pixels []byte, pals []byte, pxidx int) *Rgb {
+	r := (*Rgb)(unsafe.Pointer(&pals[int(pixels[pxidx]) * 3]))
+
+	return r
 }
